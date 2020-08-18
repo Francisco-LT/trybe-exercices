7554e64b23d33c368a7bf84ab915ca4114a2aeef
Francisco <franciscoliratorres@gmail.com>
2020-08-18 19:12:31 -0300

    mensagem que eu gostaria

--- my-shell-scripts/bonus1.sh
+++ my-shell-scripts/bonus1.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Vou ter muito suceso na programação
+
 #primeiramente declaramos a variável WORDS, que irá equivaler à nossa lista;
 
 #em seguida utiliza-se o for,  que vai criar um looping, pegando cada valor da nossa lista (WORDS) e executar o comando em em cada um desses valores

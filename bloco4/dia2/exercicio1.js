042cd554b64710d664f72c294c98229d0baccc6f
Francisco <franciscoliratorres@gmail.com>
2020-08-28 18:49:08 -0300

    Add maior ou igual a 20

--- bloco4/dia2/exercicio1.js
+++ bloco4/dia2/exercicio1.js
@@ -4,4 +4,9 @@ for (let i = 0; i < numbers.length; i += 1) {
 soma += numbers[i];
 }
 let media = (soma / numbers.length);
-console.log(media);
+if (media > 20) {
+    console.log("valor maior que 20");
+}
+else {
+    console.log("valor menor ou igual a 20");
+}

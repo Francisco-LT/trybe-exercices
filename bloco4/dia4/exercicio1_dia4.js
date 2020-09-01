8a2645c67bd24dd9ec85b86615ecd3e6d1acb8e1
Francisco <franciscoliratorres@gmail.com>
2020-09-01 15:57:59 -0300

    Add for/in

--- bloco4/dia4/exercicio1_dia4.js
+++ bloco4/dia4/exercicio1_dia4.js
@@ -1,39 +1,11 @@
-const salarioBruto = 10000.00;
-let salarioBase;
-let valorInss;
-let valorIrpf;
-let salarioLiquido;
-let aliquotaInss;
-let aliquotaIrpf;
-
-if (salarioBruto <= 1556.94){
-    aliquotaInss = salarioBruto * 0.08;
-    salarioBase = salarioBruto - aliquotaInss;
-} else if (salarioBruto <= 2594.92) {
-    aliquotaInss = salarioBruto * 0.09;   
-    salarioBase = salarioBruto - aliquotaInss;
-} else if (salarioBruto <= 5189.82) {
-    aliquotaInss = salarioBruto * 0.11;
-    salarioBase = salarioBruto - aliquotaInss;
-} else {
-    aliquotaInss = 570.88;
-    salarioBase = salarioBruto - aliquotaInss;
-}
-if (salarioBase <= 1903.98){
-    aliquotaIrpf = 0;
-    salarioLiquido = salarioBase - aliquotaIrpf;
-} else if (salarioBase <= 2826.65) {
-    aliquotaIrpf = (salarioBase * 0.075) - 142.80;
-    salarioLiquido = salarioBase - aliquotaIrpf;
-} else if (salarioBase <= 3751.05) {
-    aliquotaIrpf = (salarioBase * 0.15) - 354.80;
-    salarioLiquido = salarioBase - aliquotaIrpf;
-} else if (salarioBase <= 4664.68) {
-    aliquotaIrpf = (salarioBase * 0.225) - 636.13;
-    salarioLiquido = salarioBase - aliquotaIrpf;
-} else {
-    aliquotaIrpf = (salarioBase * 0.275) - 869.36;
-    salarioLiquido = salarioBase - aliquotaIrpf;
-}
-
-console.log(salarioLiquido);
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+    recorrente: "Sim"
+  };
+  console.log("Bem-vinda, " + info.personagem);
+  console.log(info);
+  for (let chave in info) {
+      console.log(chave);
+  }

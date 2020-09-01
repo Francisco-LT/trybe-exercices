3e22a25a110fd4c998bf86c641d7e9c7b3c312bf
Francisco <franciscoliratorres@gmail.com>
2020-09-01 18:18:55 -0300

    Add segundo objeto

--- bloco4/dia4/exercicio1_dia4.js
+++ bloco4/dia4/exercicio1_dia4.js
@@ -4,8 +4,15 @@ let info = {
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
     recorrente: "Sim"
   };
-  console.log("Bem-vinda, " + info.personagem);
-  console.log(info);
-  for (let chave in info) {
-      console.log(info[chave]);
-  }
+  info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim"
+  };
+  console.log(info.personagem + " e " + info2.personagem);
+  console.log(info.origem + " e " + info2.origem );
+  console.log(info.nota + " e " + info2.nota);
+if (info.recorrente == info2.recorrente) {
+    console.log("Ambos recorrentes // Atenção para essa última linha!")
+} 

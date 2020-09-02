2802724b2f31eb19a57470fa794c627529560c4c
Francisco <franciscoliratorres@gmail.com>
2020-09-02 09:42:06 -0300

    Add exercicio palindromos

---
+++ bloco4/dia4/exercicio2_dia4.js
@@ -0,0 +1,24 @@
+function verificarPalindromo (palavra) {
+    //primeiramente usamos o .split, que transforma uma string em um array de letras e no fim escolhemos o que vai separae essas letras;
+    let arrayDeLetras = palavra.split("");
+    let ehPalindromo = true;
+    for (let index in arrayDeLetras) {
+        //array de letras no caso é a palavra qu escolhemos, serão comparadas letra por letra para entender se é ou não um palíndromo
+        if (arrayDeLetras[index] != palavra[(palavra.length - 1) - index]) {
+            ehPalindromo = false;
+        }
+    }
+    return ehPalindromo;
+}
+function verificarPalindromo(string) {
+    //nese ponto ele pega string, separa transformando em um array, separa novamente, reverte a ordem das letras e junta novamente.
+    let reverse = string.split("").reverse().join("");
+    if (reverse === string) {
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(verificarPalindromo('arara')); //true
+console.log(verificarPalindromo("socorrammesubinoonibusemmarrocos")); //false
+   

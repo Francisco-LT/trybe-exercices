235757c71cc2a95475212aef96154947c8bb91d2
Francisco <franciscoliratorres@gmail.com>
2020-09-02 10:22:46 -0300

    Finalização exercicio 2 e 3

--- bloco4/dia4/exercicio2_dia4.js
+++ bloco4/dia4/exercicio2_dia4.js
@@ -1,15 +1,18 @@
+// o exercicio pode ser resolvido das duas maneiras abaixo
+
 function verificarPalindromo (palavra) {
     //primeiramente usamos o .split, que transforma uma string em um array de letras e no fim escolhemos o que vai separae essas letras;
     let arrayDeLetras = palavra.split("");
     let ehPalindromo = true;
     for (let index in arrayDeLetras) {
-        //array de letras no caso é a palavra qu escolhemos, serão comparadas letra por letra para entender se é ou não um palíndromo
+        //array de letras no caso é a palavra qu escolhemos, serão comparadas letra por letra (ao contrário, a primeira, com a última, a segunda com a penúltima e assim por diante) para entender se é ou não um palíndromo
         if (arrayDeLetras[index] != palavra[(palavra.length - 1) - index]) {
             ehPalindromo = false;
         }
     }
     return ehPalindromo;
 }
+
 function verificarPalindromo(string) {
     //nese ponto ele pega string, separa transformando em um array, separa novamente, reverte a ordem das letras e junta novamente.
     let reverse = string.split("").reverse().join("");
@@ -19,6 +22,6 @@ function verificarPalindromo(string) {
         return false;
     }
 }
-console.log(verificarPalindromo('arara')); //true
-console.log(verificarPalindromo("socorrammesubinoonibusemmarrocos")); //false
+console.log(verificarPalindromo('salas')); 
+console.log(verificarPalindromo("socorrammesubinoonibusemmarrocos")); 
    

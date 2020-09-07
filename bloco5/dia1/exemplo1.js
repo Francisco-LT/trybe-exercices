9818456fe1f8ac1e80d562d89c2d869612068d5d
Francisco <franciscoliratorres@gmail.com>
2020-09-07 11:11:26 -0300

    Pimeiros exercícios seletores

---
+++ bloco5/dia1/exemplo1.js
@@ -0,0 +1,5 @@
+document.getElementById("page-title").innerText = "Pulp Fiction";
+document.getElementById("subtitle").innerText = "Time do coração";
+document.getElementById("second-paragraph").innerText = "Galo doido";
+document.getElementsByClassName("paragrafo")[0].style.color = "yellow";
+document.getElementsByTagName("h4")[0].innerText = "Time!!";

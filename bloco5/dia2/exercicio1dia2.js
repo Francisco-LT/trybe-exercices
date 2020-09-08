325e822b5cf170589d1711a830d12856242272d7
Francisco <franciscoliratorres@gmail.com>
2020-09-08 18:30:10 -0300

    Add primeiros exemplos do dia 2 bloco 5

---
+++ bloco5/dia2/exercicio1dia2.js
@@ -0,0 +1,8 @@
+document.getElementById("elementoOndeVoceEsta");
+document.querySelector("#elementoOndeVoceEsta").parentNode.style.color = "red";
+document.querySelector("#primeiroFilhoDofilho").innerText= "meu primeiro filh";
+document.querySelector("#pai").firstElementChild;
+document.getElementById("elementoOndeVoceEsta").previousElementSibling;
+document.getElementById("elementoOndeVoceEsta").nextSibling;
+document.getElementById("elementoOndeVoceEsta").nextElementSibling;
+document.querySelector("#pai").nextElementSibling

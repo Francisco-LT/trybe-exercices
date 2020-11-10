08ce6e7d68e218bef189edb15af6db7ca2930ff5
Francisco <franciscoliratorres@gmail.com>
2020-11-10 09:59:40 -0300

    atualizando repositório

---
+++ testando-meu-computador/src/component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class aboutMe extends React.Component {
+  render() {
+    return(<div>
+      <h1>Francisco</h1>
+      <p>sou de bh</p>
+    </div>)
+  }
+}
+
+export default aboutMe;

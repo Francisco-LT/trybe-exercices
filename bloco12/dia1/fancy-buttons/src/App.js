6ed104993ab9f10d2a0d0b966853d1ab47aac3d0
Francisco <franciscoliratorres@gmail.com>
2020-11-10 19:37:28 -0300

    exercícios de fixação dia 1

--- bloco12/dia1/fancy-buttons/src/App.js
+++ bloco12/dia1/fancy-buttons/src/App.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import './App.css';
 
-function fuckingButton() {
-  console.log('Fucking button')
-};
-
-function fuckingButton1() {
-  console.log('Fucking button1')
-};
-
-function fuckingButton2() {
-  console.log('Fucking button2')
-};
-
 class App extends React.Component {
+  constructor(props) {
+    super (props)
+    this.funckingClick = this.funckingClick.bind(this);
+    this.state = {
+      numberOfClicks: 0
+    }
+  }
+  
+  funckingClick() {
+    this.setState((previousState, _props) => ({
+      numberOfClicks: previousState.numberOfClicks + 1
+    }))
+  };
+  // o primeiro parâmetro de set state pega o valor do statdo anterior ao dessa atualziação
+  //cada vez que chamamos o setState ele vai enfileirar a atualização e vai funcionar de forma assíncorna
+
   render() {
-    return (
-      <div>
-    <button onClick={fuckingButton}>Fucking button</button>
-    <button onClick={fuckingButton1}>Fucking button1</button>
-    <button onClick={fuckingButton2}>Fucking button2</button>
-    </div>
-    )
+    console.log(this)
+    return <button onClick={this.funckingClick}>{this.state.numberOfClicks}</button>
   }
 }
 

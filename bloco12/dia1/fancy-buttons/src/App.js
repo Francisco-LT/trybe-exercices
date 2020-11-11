fe371b2158e477e1e5d69a9c5e79d8cfe926efd4
Francisco <franciscoliratorres@gmail.com>
2020-11-11 14:40:58 -0300

    Add dia 1

--- bloco12/dia1/fancy-buttons/src/App.js
+++ bloco12/dia1/fancy-buttons/src/App.js
@@ -14,13 +14,22 @@ class App extends React.Component {
     this.setState((previousState, _props) => ({
       numberOfClicks: previousState.numberOfClicks + 1
     }))
+    if (this.state.numberOfClicks % 2 !== 0 || this.state.numberOfClicks === 0) {
+    console.log("standard button")
+    } else {
+      console.log ("green button")
+    }
   };
   // o primeiro parâmetro de set state pega o valor do statdo anterior ao dessa atualziação
   //cada vez que chamamos o setState ele vai enfileirar a atualização e vai funcionar de forma assíncorna
-
+buttonGreen(evenOrOdd) {
+  if (evenOrOdd % 2 !== 0 || evenOrOdd === 0) {
+    return ""
+  }
+   return "greenButton"
+}
   render() {
-    console.log(this)
-    return <button onClick={this.funckingClick}>{this.state.numberOfClicks}</button>
+    return <button onClick={this.funckingClick} className={this.buttonGreen(this.state.numberOfClicks)}>{this.state.numberOfClicks}</button>
   }
 }
 

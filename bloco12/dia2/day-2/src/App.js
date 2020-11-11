93cf3b00673dd199f206293d4bde483869169cb5
Francisco <franciscoliratorres@gmail.com>
2020-11-11 19:04:34 -0300

    Add exemplo 1

--- bloco12/dia2/day-2/src/App.js
+++ bloco12/dia2/day-2/src/App.js
@@ -1,12 +1,47 @@
+import React from 'react'
 
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class Form extends React.Component {
+  constructor(props) {
+    super(props)
 
-    </div>
-  );
+    this.handleTextAreaChange = this.handleTextAreaChange.bind(this)
+
+    this.state = {
+      cidade: '',
+      idade: '',
+      porqueEscolhiEsseTime: '',
+      timeDoCoracao: '',
+    }
+  }
+
+  handleTextAreaChange(event) {
+    this.setState({ porqueEscolhiEsseTime: event.target.value })
+  }
+  render() {
+    return(
+      <form>
+        <label>Digite sua cidade</label>
+        <input name="cidade" type="text"></input>
+        <label>Digite sua idade</label>
+        <input name="idade" type="number"></input>
+      <label>Escolha o seu time!</label>
+      <select name="timeDoCoracao">
+        <option value="galo">Atlético</option>
+        <option value="porco">Palmeiras</option>
+        <option value="urubu">Flamengo</option>
+        <option value="saci">Internaconal</option>
+      </select>
+      <div>
+      <label>Porque você torce para esse time?</label>
+      <textarea 
+      name="porqueEscolhiEsseTime"
+      value={this.state.porqueEscolhiEsseTime}
+      onChange={this.handleTextAreaChange}></textarea>
+      </div>
+      </form>
+    )
+  }
 }
 
-export default App;
+export default Form

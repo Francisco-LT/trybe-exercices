29570f42b2f7107cb1f834a54e5d2bac4ce3e668
Francisco <franciscoliratorres@gmail.com>
2020-11-12 09:30:37 -0300

    add exercício de fixação com um evento usado em todos

--- bloco12/dia2/day-2/src/App.js
+++ bloco12/dia2/day-2/src/App.js
@@ -5,40 +5,76 @@ class Form extends React.Component {
   constructor(props) {
     super(props)
 
-    this.handleTextAreaChange = this.handleTextAreaChange.bind(this)
+    this.handleChange = this.handleChange.bind(this)
 
     this.state = {
       cidade: '',
       idade: '',
       porqueEscolhiEsseTime: '',
       timeDoCoracao: '',
+      vaiNoCampo: false
     }
   }
 
-  handleTextAreaChange(event) {
-    this.setState({ porqueEscolhiEsseTime: event.target.value })
+  handleChange({target}) {
+    const {name} = target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    this.setState({ 
+      [name]: value 
+    })
   }
   render() {
     return(
       <form>
+        <fieldset>
+        <div>
         <label>Digite sua cidade</label>
-        <input name="cidade" type="text"></input>
+        <input
+        name="cidade"
+        type="text"
+        value={this.state.cidade}
+        onChange={this.handleChange}>
+        </input>
+        </div>
+        <div>
         <label>Digite sua idade</label>
-        <input name="idade" type="number"></input>
+        <input
+        name="idade"
+        type="number"
+        value={this.state.idade}
+        onChange={this.handleChange}>
+        </input>
+        </div>
+        </fieldset>
+        <fieldset>
+        <div>
       <label>Escolha o seu time!</label>
-      <select name="timeDoCoracao">
+      <select name="timeDoCoracao"
+      value={this.state.timeDoCoracao}
+      onChange={this.handleChange}>
         <option value="galo">Atlético</option>
         <option value="porco">Palmeiras</option>
         <option value="urubu">Flamengo</option>
         <option value="saci">Internaconal</option>
       </select>
+      </div>
       <div>
       <label>Porque você torce para esse time?</label>
       <textarea 
       name="porqueEscolhiEsseTime"
       value={this.state.porqueEscolhiEsseTime}
-      onChange={this.handleTextAreaChange}></textarea>
+      onChange={this.handleChange}>
+      </textarea>
       </div>
+      <div>
+        <input
+        type="checkbox"
+        name="vaiNoCampo"
+        value={this.state.vaiNoCampo}
+        onChange={this.handleChange}>
+        </input>
+      </div>
+      </fieldset>
       </form>
     )
   }

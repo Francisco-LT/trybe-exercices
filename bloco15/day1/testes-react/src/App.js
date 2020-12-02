447f09ffe2d24a4213b17473dcdf166cf5067fe7
Francisco <franciscoliratorres@gmail.com>
2020-12-02 15:29:56 -0300

    add itens do dia 1

--- bloco15/day1/testes-react/src/App.js
+++ bloco15/day1/testes-react/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          algo que não aparece
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+  // App.js
+  import React from 'react';
+  import './App.css';
+  
+  function App() {
+    return (
+      <div className="App">
+        <label htmlFor="id-email">
+          Email
+        </label>
+        <input id="id-email" type="email" />
+        <input id="btn-send" type="button" data-testid="id-send" value="Enviar" />
+        <input id="btn-back" type="button" value="Voltar" />
+      </div>
+    );
+  }
+  
+  export default App;

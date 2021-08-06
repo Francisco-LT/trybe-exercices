a700296138a4100aad3f4da2b07de8ff8fa056b6
Francisco <franciscoliratorres@gmail.com>
2021-08-06 18:37:45 -0300

    alteraçoes antigas em nodeJs

--- bloco26/dia4/hello-express/index.js
+++ bloco26/dia4/hello-express/index.js
@@ -13,7 +13,41 @@ app
     res.send('hello world post');
   });
 
-app.listen(3000, function () {
+  app.get(
+    '/ping',
+    function (req, res, next) {
+      console.log('fiz alguma coisa');
+      /* Chama a próxima callback */
+      next();
+    },
+    function (req, res) {
+      /* A segunda (e última) callback envia a resposta para o cliente */
+      res.send('pong!');
+    }
+  );
+  
+  /* Também podemos utilizar um array para especificar múltiplos callbacks */
+  app.get('/hello', [
+    (req, res, next) => {
+      console.log('primeiro callback acionado');
+      next();
+    },
+    (req, res, next) => {
+      console.log('segundo callback acionado');
+      res.send('Hello, world!');
+    },
+  ]);
+
+  app.get('/teste', [
+    (req, res) => {
+      res.send('Testando, som!');
+    },
+    (req, res) => {
+      console.log('Tão me ouvindo?');
+    },
+  ]);
+
+app.listen(3001, function () {
   console.log('Example app listening on port 3000!');
 });
 
